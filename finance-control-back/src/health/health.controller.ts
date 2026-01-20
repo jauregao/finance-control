acf3da2a4d8c164e5f90f782404e27c69835b162
Amanda Jauregui <amandajauregui.dev@gmail.com>
2026-01-20 18:06:37 -0300

    [BE-Finance Control] feat: add health check module and controller for application health monitoring

---
+++ finance-control-back/src/health/health.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  HealthCheck,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+import { Public } from 'src/decorators/public.decorator';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@ApiTags('Health')
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private prismaHealth: PrismaHealthIndicator,
+    private prisma: PrismaService,
+  ) {}
+
+  @Get()
+  @Public()
+  @HealthCheck()
+  @ApiOperation({ summary: 'Check application health' })
+  @ApiResponse({ status: 200, description: 'Application is healthy' })
+  @ApiResponse({ status: 503, description: 'Application is unhealthy' })
+  check() {
+    return this.health.check([
+      () => this.prismaHealth.pingCheck('database', this.prisma),
+    ]);
+  }
+}
